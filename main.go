e26fbd7fa7c13954bcd62ec467f749345bd4d977
yakitori99 <t2.akg3@gmail.com>
2021-08-31 00:04:59 +0900

    Heroku用に、環境変数からPORT番号を取得するよう変更

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"trip-weather-backend/config"
 	"trip-weather-backend/model"
 	"trip-weather-backend/route"
@@ -10,7 +11,14 @@ func main() {
 	// DBを初期化
 	model.InitDB()
 
+	// for Heroku 環境変数からポート番号を取得
+	var port string = os.Getenv("PORT")
+	// for Local 環境変数に"PORT"がない場合、configの値を利用
+	if port == "" {
+		port = config.PORT
+	}
+
 	// routerの初期化、起動
 	router := route.InitRoute()
-	router.Logger.Fatal(router.Start(":" + config.PORT))
+	router.Logger.Fatal(router.Start(":" + port))
 }
