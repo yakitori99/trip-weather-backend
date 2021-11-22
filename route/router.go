5005f2ec0d61f92922543cfba7e3420807b9e61b
yakitori99 <t2.akg3@gmail.com>
2021-11-22 23:21:34 +0900

    APIのURLを変更

--- route/router.go
+++ route/router.go
@@ -22,20 +22,20 @@ func InitRoute() *echo.Echo {
 
 	// today-1 ~ +7のdatetimeをstrにしたデータを返す
 	// クライアントサイドの時間は信用できない可能性があるため、サーバサイドで時間を取得する
-	e.GET("/get_datetimes", api.GetDatetimes())
+	e.GET("/datetimes", api.GetDatetimes())
 
 	// pref一覧を返す
-	e.GET("/get_prefs", api.GetPrefs())
+	e.GET("/prefs", api.GetPrefs())
 
 	// city一覧を返す
-	e.GET("/get_cities", api.GetCities())
+	e.GET("/cities", api.GetCities())
 	// pref_codeで検索しcity一覧を返す
-	e.GET("/get_cities/:pref_code", api.GetCitiesByPrefCode())
+	e.GET("/cities/by/:pref_code", api.GetCitiesByPrefCode())
 
 	// city_codeを用いて、現在地のWeatherHistory, WeatherForcast情報を返す
-	e.GET("/get_weather_from/:city_code", api.GetWeatherFromByCityCode())
+	e.GET("/weather_from/:city_code", api.GetWeatherFromByCityCode())
 	// city_codeを用いて、目的地のWeatherForcast情報を返す
-	e.GET("/get_weather_to/:city_code", api.GetWeatherToByCityCode())
+	e.GET("/weather_to/:city_code", api.GetWeatherToByCityCode())
 
 	// jsonを受け取り、favoritesテーブルに対しINSまたはUPDする
 	e.POST("/favorites", api.CreateFavoriteFromJson())
